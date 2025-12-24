ed09876e37ca07cdb7972a3a39f1e01988231346
Barsha-Pradhan <barshapradhan1106@gmail.com>
2025-12-24 19:18:11 +0530

    Update Anagram.java

---
+++ Anagram.java
@@ -0,0 +1,23 @@
+class Anagram {
+    public static boolean isanagram(String s,String t){
+        if(s.length()!=t.length()){
+            return false;
+        }
+        else{
+            int count[]= new int [26];
+            for(int i=0;i<s.length();i++){
+                count[s.charAt(i)-'a']++;
+            }
+            for(int i=0;i<t.length();i++){
+                count[t.charAt(i)-'a']--;
+            }
+            for(int i=0;i<count.length();i++){
+                if(count[i]!=0){
+                    return false;
+                }
+            }
+        }
+    return true;
+    }
+    
+}
